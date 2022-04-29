15f01a84873db4466ac5f9316f5cbca9a709db5d
Eduardo <eduundav@gmail.com>
2022-04-29 18:46:34 -0300

    fase 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { InteresesComponent } from './intereses/intereses.component';
+import { DisponibilidadComponent } from './disponibilidad/disponibilidad.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    InteresesComponent,
+    DisponibilidadComponent,
+    EducacionComponent,
+    ProyectosComponent,
+    ContactoComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
